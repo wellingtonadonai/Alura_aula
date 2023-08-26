6360bcd901e5d0f120e00cb1daa953361b317415
wellington <wellington.h.s.oliveira@hotmail.com>
2023-08-26 16:29:23 -0300

    exemplos da aula 1 alura

---
+++ text.js
@@ -0,0 +1,9 @@
+const text1 = "ola mundo!"
+const text2 = "ola mundos!"
+
+console.log(text1)
+
+const numeroNovo = 10
+const numeroVelho = 10
+
+console.log(numeroNovo === numeroVelho)
---
+++ type-number.js
@@ -0,0 +1,19 @@
+//type number 
+
+const meuNumero = 2; 
+
+const primeiroNumero = 1;
+const segundoNumero = 4
+
+const operacaoMatematica = primeiroNumero + segundoNumero;
+
+
+
+//ponto flutuante
+
+const pontoFlutuante = 3.3;
+const pontoFlutuanteSemZero = .5;
+
+const novaOperacao = pontoFlutuante / pontoFlutuanteSemZero;
+console.log(novaOperacao);
+
